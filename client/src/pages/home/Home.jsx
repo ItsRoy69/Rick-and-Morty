57eab952be6fd399ee408568d1276736adbb8b04
itsroy69 <jyotirmoyroy649@gmail.com>
2023-12-31 00:32:21 +0530

    added link
    Signed-off-by: itsroy69 <jyotirmoyroy649@gmail.com>

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     const fetchCharacters = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:5000/api/characters"
+          "https://rick-and-morty-al6l.onrender.com//api/characters"
         );
         setCharacters(response.data);
       } catch (error) {
